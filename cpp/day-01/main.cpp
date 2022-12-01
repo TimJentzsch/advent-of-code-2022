5310202213f71fa0e303767ad6fd4b879a059e77
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-12-01 11:36:21 +0100

    Add day 1 part 1 solution

--- cpp/day-01/main.cpp
+++ cpp/day-01/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(int argc, char const *argv[])
 {
-  std::cout << "Hello world!\n";
+  std::ifstream inputFile;
+  inputFile.open("input.txt");
+
+  int maxCalories = 0;
+  int curCalories = 0;
+
+  std::string line;
+
+  while (!inputFile.eof())
+  {
+    std::getline(inputFile, line);
+
+    // An empty line means that the next inventory starts
+    if (line.length() == 0)
+    {
+      maxCalories = std::max(maxCalories, curCalories);
+      curCalories = 0;
+    }
+    else
+    {
+      int calories = std::stoi(line);
+      curCalories += calories;
+    }
+  }
+
+  inputFile.close();
+
+  // Print solution
+  std::cout << maxCalories << "\n";
+
   return 0;
 }
