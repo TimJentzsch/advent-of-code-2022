e259ac64cb1e754556460ad6804b0dcebd3c56d6
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-12-01 11:45:42 +0100

    Add day 01 part 2 solution

--- cpp/day-01/main.cpp
+++ cpp/day-01/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <bits/stdc++.h>
 
 int main(int argc, char const *argv[])
 {
   std::ifstream inputFile;
   inputFile.open("input.txt");
 
-  int maxCalories = 0;
+  std::vector<int> elfCalories;
   int curCalories = 0;
 
   std::string line;
@@ -19,7 +21,7 @@ int main(int argc, char const *argv[])
     // An empty line means that the next inventory starts
     if (line.length() == 0)
     {
-      maxCalories = std::max(maxCalories, curCalories);
+      elfCalories.push_back(curCalories);
       curCalories = 0;
     }
     else
@@ -31,8 +33,15 @@ int main(int argc, char const *argv[])
 
   inputFile.close();
 
-  // Print solution
-  std::cout << maxCalories << "\n";
+  // Sort the calories in descending order
+  std::sort(elfCalories.begin(), elfCalories.end(), std::greater<int>());
+
+  // Part 1 solution
+  std::cout << "Maximum calories: " << elfCalories[0] << "\n";
+
+  // Part 2 solution
+  int topThreeCalories = elfCalories[0] + elfCalories[1] + elfCalories[2];
+  std::cout << "Top 3 calories: " << topThreeCalories << "\n";
 
   return 0;
 }
